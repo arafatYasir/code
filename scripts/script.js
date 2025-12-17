ec37021a49f28fd754869714448d2456a18a0812
iamrafsanrafi <arafat.yasir1305@gmail.com>
2025-12-17 09:20:53 +0600

    more pages done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log("HELLO")
+// Handling Navigation dropdowns
+const navOptions = document.querySelectorAll('.oneupmenu-nav-options li');
+
+navOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        navOptions.forEach(option => option.classList.remove('active'));
+        option.classList.add('active');
+    })
+})
